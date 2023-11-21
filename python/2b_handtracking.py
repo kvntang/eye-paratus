984648310cce9ee7d54911a7219f26b93e4a1b7b
Kevin <kevintang1509@gmail.com>
2023-11-20 22:27:49 -0500

    renamed files

---
+++ python/2b_handtracking.py
@@ -0,0 +1,8 @@
+#MachineGaze Demo 2
+#Handtracking
+#1. detect hand position
+#2. move rotation motor
+#3. move tilt motor so hand is in mid range (20% - 80% height) of screen
+
+import cv2
+import mediapipe as mp
